d7a45b69aae9ac446824b769e746d184dddf132d
Jesus Marin <jesusangelm@gmail.com>
2014-07-15 11:47:41 -0430

    initial commit

---
+++ init.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# Notes
+# Simple CLI application for note taking
+# ======================================
+
+# constante que almacena el directorio actual de la aplicacion,
+APP_ROOT = File.dirname(__FILE__)
+
+# carga la carpeta lib y sus contenidos para luego requerirlos  y poder
+# usarlos en la aplicacion.
+$:.unshift( File.join(APP_ROOT, "lib") )
+require "cli"
+
